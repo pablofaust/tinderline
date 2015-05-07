e46efdbae783c6277b2c8865b789cf0b4f863d95
Pablo Faust <pabfaust@gmail.com>
2015-05-07 15:36:09 -0500

    Phrases enregistrées

--- home/counter.php
+++ home/counter.php
@@ -1,5 +1,6 @@
 <div class="container-fluid counter">
 
-	<h2><strong><?php include $route.'/home/count.php' ?></strong> phrases générées</h2>
+	<h2><strong><?php include $route.'/home/count.php' ?></strong> phrases affichées</h2>
+	<h2><strong><?php include $route.'/home/total-count.php' ?></strong> phrases enregistrées</h2>
 
 </div>
