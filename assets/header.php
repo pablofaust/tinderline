1b678384df15444a16e9df11e90ab7764c06543d
Pablo Faust <pabfaust@gmail.com>
2015-05-07 00:33:16 -0500

    Boostrap https

--- assets/header.php
+++ assets/header.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link type="text/css" rel="stylesheet" href="<?php echo $url ?>css/bootstrap.css" />
+  <link type="text/css" rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">" />
   <link type="text/css" rel="stylesheet" href="<?php echo $url ?>css/style.css" />
   <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
   <title>Tinderline</title>
