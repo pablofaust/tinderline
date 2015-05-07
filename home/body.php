8adf14f54e8f3b42cd261b1916cc99f22cafc364
Pablo Faust <pabfaust@gmail.com>
2015-05-07 14:57:44 -0500

    Responsive

--- home/body.php
+++ home/body.php
@@ -2,7 +2,8 @@
 
 	<div class="generator">
 		<h1><?php include $route.'/home/generator.php' ?></h1>
-		<p>Appuie sur <strong>espace</strong> pour une autre phrase.</p>
+		<p class="hidden-sm hidden-md hidden-lg"><strong>Recharge</strong> la page pour une autre phrase.</p>
+		<p class="hidden-xs">Appuie sur <strong>espace</strong> ou <strong>recharge la page</strong> pour une autre phrase.</p>
 	</div>
 
 
